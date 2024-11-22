2c17a8e6b3a6643ded20d1bf88d351d98a9ba656
David Cerón <64936862+ZeroG234@users.noreply.github.com>
2024-11-22 15:08:06 -0500

    Correcciones
    Según los errores presentados en la prueba automática, se corrigió la clase asignatura. Se quitó un atributo artificial creado, y se hicieron las debidas correcciones en el atributo salón

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,9 +1,8 @@
 class Asignatura:
 
-    def __init__(self, nombre, salon = 0, modalidad = 'remoto'):
+    def __init__(self, nombre, salon = 'remoto', modalidad = 'remoto'):
         self._nombre = nombre
         self._salon = salon
-        self._modalidad = modalidad
 
     def __str__(self):
-        return self._nombre + ' ' + self._modalidad
+        return self._nombre + ' ' + self._salon
