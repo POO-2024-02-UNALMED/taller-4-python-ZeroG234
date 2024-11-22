46244cfbfd514877c00003170120dfe10ecdbf28
David Cerón <64936862+ZeroG234@users.noreply.github.com>
2024-11-22 15:00:06 -0500

    Correciones
    Se hicieron las correciones necesarias para que el código se ejecutara de manera que se imprimiese lo pedido.

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,9 @@
 class Asignatura:
 
-    def __init__(self, nombre, salon):
+    def __init__(self, nombre, salon = 0, modalidad = 'remoto'):
         self._nombre = nombre
         self._salon = salon
+        self._modalidad = modalidad
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        return self._nombre + ' ' + self._modalidad
